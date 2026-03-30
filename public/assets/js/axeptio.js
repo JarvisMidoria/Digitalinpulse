63a234cbcd47f1c04af0cd8118648d583fe8c790
Maddyness <maddyness@192.168.1.4>
2026-03-30 18:58:26 +0100

    Add official Axeptio integration

---
+++ public/assets/js/axeptio.js
@@ -0,0 +1,21 @@
+window.axeptioSettings = {
+  clientId: "69cab0bef1fbc236f0e8ed86",
+  cookiesVersion: "d2617437-674e-4d4c-befc-5b7cd31dfbc2",
+  googleConsentMode: {
+    default: {
+      analytics_storage: "denied",
+      ad_storage: "denied",
+      ad_user_data: "denied",
+      ad_personalization: "denied",
+      wait_for_update: 500,
+    },
+  },
+};
+
+(function (d, s) {
+  var t = d.getElementsByTagName(s)[0],
+    e = d.createElement(s);
+  e.async = true;
+  e.src = "https://static.axept.io/sdk.js";
+  t.parentNode.insertBefore(e, t);
+})(document, "script");
